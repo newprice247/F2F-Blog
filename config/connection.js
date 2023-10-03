199c996260f09a70b734a90104e9014b3ceac1c5
newprice247 <newprice247@gmail.com>
2023-10-02 20:38:39 -0400

    connected to heroku, jawsdb env. variable included in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 require("dotenv").config();
 
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
