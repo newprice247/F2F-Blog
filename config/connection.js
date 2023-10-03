7be41e1a48716221d8507edc42aada17d5d09503
newprice247 <newprice247@gmail.com>
2023-10-02 20:14:43 -0400

    trying to deploy to heroku

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
