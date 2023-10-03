b5ac31d695cc5930ee060dc26c697e9014967e38
kwaters3 <knickler3@gmail.com>
2023-10-03 16:40:29 -0400

    added function for user to input data with link, when link is clicked - it will navigate to the desired page

--- public/js/resources.js
+++ public/js/resources.js
@@ -6,17 +6,28 @@ function handleSubmit(event) {
   const url = document.getElementById('urlLink').value;
   const tagName = document.getElementById('tagID').value;
 
+
   // Create a display message
   const displayMessage = `
       <h3>Submitted Data:</h3>
       <p><strong>Comment:</strong> ${comment}</p>
-      <p><strong>URL:</strong> < ${url}</p>
+      <p><strong>URL:</strong> <a href="${url}" target="_blank" id="urlLinkDisplay">${url}</a></p>
       <p><strong>Tag Name:</strong> ${tagName}</p>
-  `;
+      `;
+
 
   // Display the message in the "displayArea" div
   document.getElementById('displayArea').innerHTML = displayMessage;
 }
+
+
+const urlLinkDisplay = document.getElementById('urlLinkDisplay');
+    if (urlLinkDisplay) {
+        urlLinkDisplay.addEventListener('click', () => {
+            window.open(url);
+        });
+    }
+
 
 // Add an event listener to the form
 const form = document.getElementById('myForm');
