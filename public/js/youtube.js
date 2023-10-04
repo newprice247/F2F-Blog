923a210e4c94772e120fa875f6f9a0f0d02ca8bf
Abigail Burtley <abigailtop95@yahoo.com>
2023-10-04 17:51:03 -0400

    fix variable scopes of youtube js file for uploading videos

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,7 +1,13 @@
-const videoSection =document.querySelector('.fetched-videos')
-const apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUxcQ_tkfXcXe3V2bgrntDGw&key=AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o`;
+let videoSectionHTML = '';
 
-fetch(apiUrl)
+let secondVideoSectionHTML = '';
+
+  const fetchedVideosContainer = document.querySelector('.fetched-videos'); 
+const apiUrl1 = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUxcQ_tkfXcXe3V2bgrntDGw&key=AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o`;
+const apiUrl2 = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUmqcVuqFT016HusUKVLTQoA&key=AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o`;
+const apiUrl3 = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU0gd2JJQK1R9_lRJWkkuvqg&key=AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o`;
+
+fetch(apiUrl1)
   .then((response) => {
 
     if (!response.ok) {
@@ -12,7 +18,7 @@ fetch(apiUrl)
   })
   .then((data) => {
     console.log(data);
-    let videoSectionHTML = '';
+   
     data.items.forEach(el => {
         videoSectionHTML += `
         <div class="video-card">
@@ -21,19 +27,78 @@ fetch(apiUrl)
             <h3>${el.snippet.title}</h3>
         </a>
     </div>`;
+
     
-    });
-
-
-    videoSection.innerHTML = videoSectionHTML;
-
-   
+  fetchedVideosContainer.innerHTML = videoSectionHTML;
     
+});
   })
   .catch((error) => {
     console.error('Problem fetching videos:', error);
   });
 
+  
+
+  fetch(apiUrl2)
+  .then((response) => {
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    return response.json();
+  }) 
+  .then((data) => {
+    console.log(data); 
+
+    data.items.forEach(el => {
+        secondVideoSectionHTML += `
+        <div class="video-card">
+        <a target="_blank" href="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}">
+            <img src="${el.snippet.thumbnails.maxres.url}" />
+            <h3>${el.snippet.title}</h3>
+        </a>
+    </div>`;
+    
+    }); 
+    fetchedVideosContainer.innerHTML += secondVideoSectionHTML;
+})
+  .catch((error) => {
+    console.error('Problem fetching videos:', error);
+  });
+  
+
+  fetch(apiUrl3)
+  .then((response) => {
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    return response.json();
+  }) 
+  .then((data) => {
+    console.log(data); 
+
+    let thirdVideoSectionHTML = '';
+    data.items.forEach(el => {
+        thirdVideoSectionHTML += `
+        <div class="video-card">
+        <a target="_blank" href="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}">
+            <img src="${el.snippet.thumbnails.default.url}" />
+            <h3>${el.snippet.title}</h3>
+        </a>
+    </div>`;
+    fetchedVideosContainer.innerHTML += secondVideoSectionHTML + thirdVideoSectionHTML;
+    
+    })
+    
+  })
+  .catch((error) => {
+    console.error('Problem fetching videos:', error);
+  });
+  
+  
 /*
 channelId = UCxcQ_tkfXcXe3V2bgrntDGw
 
@@ -44,4 +109,13 @@ uploads ID:  AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o
 url: https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUxcQ_tkfXcXe3V2bgrntDGw&key=AIzaSyDzzPbwlQWOA_qRC8F2pm6xG6W91d6b-2o
 
 https://www.youtube.com/watch?v=KGQ7tm9a6Tk&t=3s
+
+channel2 id : UCmqcVuqFT016HusUKVLTQoA
+UPLOADSID: UUmqcVuqFT016HusUKVLTQoA
+
+youtube: https://www.youtube.com/watch?v=86DGvUC66uY
+*/
+
+/*
+channel id : 4hQRanjObqo
 */
