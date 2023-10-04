a63a10aca4a3b85b679e69c9461024add387a002
kwaters3 <knickler3@gmail.com>
2023-10-04 13:00:28 -0400

    updating javascript

--- public/js/resources.js
+++ public/js/resources.js
@@ -27,14 +27,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const resourceDiv = document.createElement('div');
     resourceDiv.className = 'resource-item';
     resourceDiv.innerHTML = `
+      <h3>New Resource Added:</h3>
       <p><strong>Comment:</strong> ${data.comment}</p>
-      <p><strong>URL:</strong> ${data.url}</p>
+      <p><strong>URL:</strong> <a href="${data.url}" target="_blank" id="urlLinkDisplay">${data.url}</a></p>
       <p><strong>Tag:</strong> ${data.tag}</p>
     `;
 
     displayArea.appendChild(resourceDiv);
   }
 
+  // clears the form after submitting input
   function clearFormFields() {
     document.getElementById('commentBox').value = '';
     document.getElementById('urlLink').value = '';
@@ -44,44 +46,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-
-
-
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  // Get the values from the form fields
-  const comment = document.getElementById('commentBox').value;
-  const url = document.getElementById('urlLink').value;
-  const tagName = document.getElementById('tagID').value;
-
-
-  // Create a display message
-  const displayMessage = `
-      <h3>Submitted Data:</h3>
-      <p><strong>Comment:</strong> ${comment}</p>
-      <p><strong>URL:</strong> <a href="${url}" target="_blank" id="urlLinkDisplay">${url}</a></p>
-      <p><strong>Tag Name:</strong> ${tagName}</p>
-      `;
-
-
-  // Display the message in the "displayArea" div
-  document.getElementById('displayArea').innerHTML = displayMessage;
-}
-
-
-const urlLinkDisplay = document.getElementById('urlLinkDisplay');
-    if (urlLinkDisplay) {
-        urlLinkDisplay.addEventListener('click', () => {
-           window.open(url, '_blank');
-        });
-    }
-
-
-// Add an event listener to the form
-const form = document.getElementById('myForm');
-if (form) {
-  form.addEventListener('submit', handleSubmit);
-}
