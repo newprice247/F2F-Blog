20e24c7e1021f17a6556fed9fdd7f4811b8fbcae
newprice247 <newprice247@gmail.com>
2023-10-03 21:40:17 -0400

    m

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', async (req, res) => {
+    try {
+        // Get all users, excluding their password
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] }
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET /api/users/":id"
+// router.get('/:id', async (req, res) => {
+//     try {
+//         // Get a single user by id, excluding their password
+//         const userData = await User.findByPk(req.params.id, {
+//             attributes: { exclude: ['password'] }
+//         });
+//         // If no user is found, return an error
+//         if (!userData) {
+//             res.status(404).json({ message: 'No user found with this id!' });
+//             return;
+//         }
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+// // POST /api/users
+// router.post('/', async (req, res) => {
+//     try {
+//         // Create a new user
+//         const userData = await User.create(req.body);
+//         req.session.save(() => {
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
+//             res.status(200).json(userData);
+//         });
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
+
+// // DELETE /api/users/":id"
+// router.delete('/:id', async (req, res) => {
+//     try {
+//         // Delete a user by id
+//         const userData = await User.destroy({
+//             where: { id: req.params.id }
+//         });
+//         // If no user is found, return an error
+//         if (!userData) {
+//             res.status(404).json({ message: 'No user found with this id!' });
+//             return;
+//         }
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connection')
 const path = require('path')
 
 // const exphbs = require('express-handlebars')
-// const routes = require('./controllers')
+const routes = require('./controllers')
 
 const app = express()
 const PORT = process.env.PORT || 3001;
@@ -33,7 +33,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use(routes)
+app.use(routes)
 
 
 app.get('/login.html', (req, res) => {
