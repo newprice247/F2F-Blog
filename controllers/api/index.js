084c8c6fa65607425f077f61ab9d88511cd58c6c
newprice247 <newprice247@gmail.com>
2023-10-04 11:34:27 -0400

    add default content GET route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const contentRoutes = require('./contentRoutes');
 
 router.use('/users', userRoutes);
+router.use('/content', contentRoutes);
 
 module.exports = router;
