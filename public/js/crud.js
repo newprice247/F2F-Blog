660d7f5df29e30da59b5df4f64312fdf56c7bc17
Abigail Burtley <abigailtop95@yahoo.com>
2023-10-05 23:08:03 -0400

    add function to add post and send the post data back to backend

--- public/js/crud.js
+++ public/js/crud.js
@@ -1,1 +1,30 @@
-document
+function addPost(e) {;
+
+e.preventDefault();
+
+const postTitle = document.getElementById('title');
+const postContent = document.getElementById('content');
+
+const postData = {
+    Title: postTitle,
+    Content: postContent,
+    createdAt: new Date().toISOString(),
+
+};
+
+fetch('/api/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+
+})
+.then(res => res.json())
+.then(data => {
+    console.log('Post added:', data)
+    alert('New post added!');
+
+})
+.catch(err => console.error('oops sorry post could not be added. Error!'));
+}
