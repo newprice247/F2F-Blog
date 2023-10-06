e5561c7ad0e97f2e225d9e3080f84fe7426f435f
newprice247 <newprice247@gmail.com>
2023-10-06 14:05:05 -0400

    posts are going through and page is reloaded to immediatly show the changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -75,6 +75,18 @@ router.get('/profile', async (req, res) => {
     }
 });
 
+router.get('/postHistory', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Content }]
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.post('/register', async (req, res) => {
     try {
         console.log(req.body)
