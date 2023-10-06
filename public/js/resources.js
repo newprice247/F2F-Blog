c199a76cfdd8a69f873e48b8d12fffa89227a1e0
kwaters3 <knickler3@gmail.com>
2023-10-05 21:54:55 -0400

    added search by tag name buttons

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,10 +1,3 @@
-// clears the form after submitting input
-function clearFormFields() {
-  document.getElementById('commentBox').value = '';
-  document.getElementById('urlLink').value = '';
-  document.getElementById('tagID').value = 'none';
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   const getResources = () => {
     fetch('/api/resources')
@@ -13,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('getResources', data);
         for (let i = 0; i < data.length; i++) {
           $('#displayArea').append(`
-            <div class="resource-item">
+            <div class="resource-item" data-tag="${data[i].tag}"> <!-- Add data-tag attribute -->
               <h3>Resource:</h3>
               <p><strong>Comment:</strong> ${data[i].comment}</p>
               <p><strong>URL:</strong> <a href="${data[i].url}" target="_blank" id="urlLinkDisplay">${data[i].url}</a></p>
@@ -38,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   };
 
-
   const form = document.getElementById('myForm');
   const displayArea = document.getElementById('displayArea');
 
@@ -60,28 +52,63 @@ document.addEventListener('DOMContentLoaded', function () {
     displayResource(resourceData);
     postResource(commentBox, urlLink, tagID);
 
-
     // Clear the form fields
     clearFormFields();
   });
-  
+
   // This adds new data
-function displayResource(data) {
-  const resourceDiv = document.createElement('div');
-  resourceDiv.className = 'resource-item';
-  resourceDiv.innerHTML = `
-    <h3>New Resource Added:</h3>
-    <p><strong>Comment:</strong> ${data.comment}</p>
-    <p><strong>URL:</strong> <a href="${data.url}" target="_blank" id="urlLinkDisplay">${data.url}</a></p>
-    <p><strong>Tag:</strong> ${data.tag}</p>
-  </div>`;
+  function displayResource(data) {
+    const resourceDiv = document.createElement('div');
+    resourceDiv.className = 'resource-item';
+    resourceDiv.setAttribute('data-tag', data.tag); // Add data-tag attribute
+    resourceDiv.innerHTML = `
+      <h3>New Resource Added:</h3>
+      <p><strong>Comment:</strong> ${data.comment}</p>
+      <p><strong>URL:</strong> <a href="${data.url}" target="_blank" id="urlLinkDisplay">${data.url}</a></p>
+      <p><strong>Tag:</strong> ${data.tag}</p>
+    </div>`;
 
-  displayArea.insertBefore(resourceDiv, displayArea.firstChild);
-}
+    displayArea.insertBefore(resourceDiv, displayArea.firstChild);
+  }
+
+  // clears the form after submitting input
+  function clearFormFields() {
+    document.getElementById('commentBox').value = '';
+    document.getElementById('urlLink').value = '';
+    document.getElementById('tagID').value = 'none';
+  }
 
 
+  const tagButtons = document.querySelectorAll('.tag-button');
+  tagButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      const selectedTag = button.getAttribute('data-tag');
+      const resources = document.querySelectorAll('.resource-item');
+
+      for (let i = 0; i < resources.length; i++) {
+        const resourceTag = resources[i].getAttribute('data-tag').toLowerCase();
+        if (selectedTag === 'all' || resourceTag === selectedTag) {
+          resources[i].style.display = 'block'; // Show the button
+        } else {
+          resources[i].style.display = 'none'; // Hide the button
+        }
+      }
+    });
+  });
+
+  // // Search by tag functionality
+  // const tagSearchButton = document.getElementById('tagSearchButton');
+  // tagSearchButton.addEventListener('click', function () {
+  //   const tagSearchText = document.getElementById('tagSearchInput').value.toLowerCase();
+  //   const resources = document.querySelectorAll('.resource-item');
+
+  //   for (let i = 0; i < resources.length; i++) {
+  //     const resourceTag = resources[i].getAttribute('data-tag').toLowerCase();
+  //     if (resourceTag.includes(tagSearchText) || tagSearchText === 'all') {
+  //       resources[i].style.display = 'block';
+  //     } else {
+  //       resources[i].style.display = 'none';
+  //     }
+  //   }
+  // });
 });
-
-
-
-
