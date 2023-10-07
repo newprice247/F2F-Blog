3fa6d1db7b629afe76c8897546c5f3421a0ad2df
Nathaniel Price <118676646+newprice247@users.noreply.github.com>
2023-10-07 08:59:04 -0400

    Update crud.js

--- public/js/crud.js
+++ public/js/crud.js
@@ -1,7 +1,7 @@
 
 const postButton = document.getElementById("post-button");
 
-// import { getContent } from './blog.js';
+import { getContent } from './blog.js';
 
 function addPost(e) {
     const title = document.querySelector(".postTitle").value;
@@ -25,7 +25,7 @@ function addPost(e) {
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
-            // getContent();
+            getContent();
             document.location.replace('/crud');
             
         })
