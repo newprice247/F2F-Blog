93fd164b7cb22cbc33c80c069a44f023e27efd69
newprice247 <newprice247@gmail.com>
2023-10-10 11:44:50 -0400

    removed bug from crud.js

--- controllers/web.js
+++ controllers/web.js
@@ -7,7 +7,6 @@ const multer = require('multer');
 
 const Image = require('../models/Image');
 
-
 const imageFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
         cb(null, true);
@@ -57,7 +56,6 @@ const uploadFiles = async (req, res) => {
         return res.send(`Error when trying upload images: ${error}`);
     }
 }
-
 
 router.get("/upload", (req, res) => {
     res.sendFile(path.join(`${__dirname}/../views/images.html`))
